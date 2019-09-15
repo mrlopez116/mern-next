f2713d15c14182bfb016fe099d5422c05505abb0
Mario Lopez <mariolopez@Marios-MacBook-Pro-2.local>
2019-09-15 10:54:31 -0700

    Webpack configuration progress

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const withCSS = require('@zeit/next-css')
- const webpack = require("webpack");
+const webpack = require("webpack");
 module.exports = withCSS({
     cssLoaderOptions: {
-        url: false
+        url: true
     }
 })
 
@@ -29,62 +29,52 @@ module.exports = {
         //         }
         //     }]
         // })
-        config.module.rules.push({
-            rules: [
-                // Babel loader, will use your project’s .babelrc
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                },
-                // Other loaders that are needed for your components
-                {
-                    test: /\.css$/,
-                    loader: 'style-loader!css-loader?modules'
-                },
-                {
-                    test: /\.(jpg|png|svg)$/,
-                    use: {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 25000
-                        }
+        config.module.rules.push(
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 25000
                     }
                 }
-            ]
-        })
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            })
         // Example using webpack option
-        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+        //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
         return config
-    },
-    webpackDevMiddleware: config => {
-        // Perform customizations to webpack dev middleware config
-        // Important: return the modified config
-        config.module.push({
-            rules: [
-                // Babel loader, will use your project’s .babelrc
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader'
-                },
-                // Other loaders that are needed for your components
-                {
-                    test: /\.css$/,
-                    loader: 'style-loader!css-loader?modules'
-                },
-                {
-                    test: /\.(jpg|png|svg)$/,
-                    use: {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 25000
-                        }
-                    }
-                }
-            ]
-        })
+    }
+    // webpackDevMiddleware: config => {
+    //     // Perform customizations to webpack dev middleware config
+    //     // Important: return the modified config
+    //     config.module.push({
+    //         rules: [
+    //             // Babel loader, will use your project’s .babelrc
+    //             {
+    //                 test: /\.jsx?$/,
+    //                 exclude: /node_modules/,
+    //                 loader: 'babel-loader'
+    //             },
+    //             // Other loaders that are needed for your components
+    //             {
+    //                 test: /\.css$/,
+    //                 loader: 'style-loader!css-loader?modules'
+    //             },
+    //             {
+    //                 test: /\.(jpg|png|svg)$/,
+    //                 use: {
+    //                     loader: 'url-loader',
+    //                     options: {
+    //                         limit: 25000
+    //                     }
+    //                 }
+    //             }
+    //         ]
+    //     })
 
-        return config
-    },
-}
+    //     return config
+    // },
+};
